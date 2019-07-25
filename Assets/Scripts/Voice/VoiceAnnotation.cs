90934861111767dff884dc7cf4028731a8cb5d74
Joseph Byrne <byrne.joseph+github@gmail.com>
2019-07-25 16:57:28 +0930

    Add dictation and voice commands using Windows Speech API

---
+++ Assets/Scripts/Voice/VoiceAnnotation.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceAnnotation : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public TMPro.TextMeshProUGUI statusText;
+    public TMPro.TextMeshProUGUI hypothesisText;
+    public TMPro.TextMeshProUGUI resultText;
+
+    [Header("Configuration Options")]
+    public ConfidenceLevel confidenceThreshold;
+
+    DictationRecognizer dictationRecognizer;
+    VoiceRecorder recorder;
+    bool dictationStarted;
+    List<DictationResult> transcription;
+
+    KeywordListener keywordListener;
+
+    struct DictationResult
+    {
+        public string text;
+        public ConfidenceLevel confidence;
+
+        public DictationResult(string t, ConfidenceLevel c)
+        {
+            text = t;
+            confidence = c;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialiseVoiceRecognition();
+        recorder = GetComponent<VoiceRecorder>() ?? gameObject.AddComponent<VoiceRecorder>();
+        keywordListener = FindObjectOfType<KeywordListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dictationStarted)
+        {
+            statusText.SetText(dictationRecognizer.Status.ToString());
+        }
+    }
+
+    public async void ToggleDictationPressed(bool dictationEnabled)
+    {
+        await ToggleDictationPressedAsync(dictationEnabled);
+    }
+
+    async Task ToggleDictationPressedAsync(bool enableDictation)
+    {
+        dictationStarted = enableDictation;
+        if (dictationStarted)
+        {
+            keywordListener?.StopListening();
+            await Task.Delay(100); // wait for phraserecognitionsystem to turn off
+            dictationRecognizer.Start();
+            recorder.StartRecording();
+        }
+        else
+        {
+            dictationRecognizer.Stop();
+            recorder.StopRecording();
+            await Task.Delay(300);
+            // Save recorded audio to disk
+            recorder.SaveRecording();
+
+            keywordListener?.StartListening();
+        }
+    }
+
+    void InitialiseVoiceRecognition()
+    {
+        dictationRecognizer = new DictationRecognizer(confidenceThreshold, DictationTopicConstraint.Dictation);
+
+        dictationRecognizer.DictationComplete += DictationRecognizer_DictationComplete;
+        dictationRecognizer.DictationError += DictationRecognizer_DictationError;
+        dictationRecognizer.DictationHypothesis += DictationRecognizer_DictationHypothesis;
+        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+
+        transcription = new List<DictationResult>();
+    }
+
+    private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        transcription.Add(new DictationResult(text, confidence));
+        resultText.SetText($"{text} [confidence={confidence}]");
+        statusText.SetText(dictationRecognizer.Status.ToString());
+        dictationStarted = dictationRecognizer.Status == SpeechSystemStatus.Running;
+    }
+
+    private void DictationRecognizer_DictationHypothesis(string text)
+    {
+        hypothesisText.SetText(text);
+    }
+
+    private void DictationRecognizer_DictationError(string error, int hresult)
+    {
+        dictationStarted = false;
+        statusText.SetText($"Error ({hresult}): {error}");
+    }
+
+    private void DictationRecognizer_DictationComplete(DictationCompletionCause cause)
+    {
+        Debug.Log($"Dictation complete: {cause.ToString()}");
+        dictationStarted = false;
+    }
+}
---
+++ Assets/Scripts/Voice/VoiceRecorder.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceRecorder : MonoBehaviour
+{
+    public AudioClip recording;
+    
+    int bufferLength;
+    int sampleFrequency;
+    int lastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var device in Microphone.devices)
+        {
+            Microphone.GetDeviceCaps(device, out int minFreq, out int maxFreq);
+            Debug.Log($"Device found - {device}, [{minFreq}Hz, {maxFreq}Hz]");
+        }
+
+        bufferLength = 5;
+        sampleFrequency = 44100;
+
+        lastPosition = 0;
+    }
+
+    public void StartRecording()
+    {
+        recording = Microphone.Start(null, true, bufferLength, sampleFrequency);
+        StartCoroutine(RetainAudioDataAsync());
+    }
+
+    public void StopRecording()
+    {
+        Debug.Log("Stop Recording called");
+        Microphone.End(null);
+    }
+
+    public void SaveRecording()
+    {
+        WavUtility.FromAudioClip(recording, out var path);
+        Debug.Log($"Recording saved to {path}");
+    }
+
+    private void RetainAudioData(System.IO.BinaryWriter writer)
+    {
+        // move data from circular audio buffer into list of buffers
+        var recordingPosition = Microphone.GetPosition(null);
+        var samples = lastPosition <= recordingPosition ? recordingPosition - lastPosition : recordingPosition + (recording.samples - lastPosition);
+        if (samples == 0) return;
+
+        var buffer = new float[(samples - 1) * recording.channels];
+        recording.GetData(buffer, lastPosition);
+        lastPosition = recordingPosition;
+
+        foreach (float f in buffer)
+            writer.Write(f);
+    }
+
+    private IEnumerator RetainAudioDataAsync()
+    {
+        using (var stream = new System.IO.MemoryStream())
+        using (var writer = new System.IO.BinaryWriter(stream))
+        {
+            while (Microphone.IsRecording(null))
+            {
+                RetainAudioData(writer);
+                yield return null;
+            }
+
+            // Clean up, do one last retain
+            RetainAudioData(writer);
+
+            // Create stitched audio file
+            var samples = stream.Length / sizeof(float);
+            var data = new float[samples];
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            using (var reader = new System.IO.BinaryReader(stream))
+            {
+                for (int i = 0; i < samples; ++i)
+                {
+                    data[i] = reader.ReadSingle();
+                }
+            }
+
+            recording = AudioClip.Create("Complete recording", (int)samples, 1, sampleFrequency, false);
+            recording.SetData(data, 0);
+
+            Debug.Log("Recording finished");
+        }
+
+    }
+}
